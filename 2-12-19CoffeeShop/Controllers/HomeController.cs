ff9e2c4f5fa3dcca6e919c7b8292c7b330ad88d8
Jacky Chan <cjacky624@gmail.com>
2019-02-14 00:08:15 -0500

    Initial commit

---
+++ 2-12-19CoffeeShop/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using _2_12_19CoffeeShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _2_12_19CoffeeShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Result(User u)
+        {
+            ViewBag.FirstName = u.FirstName;
+            ViewBag.LastName = u.LastName;
+            ViewBag.Email = u.Email;
+            ViewBag.PhoneNumber = u.PhoneNumber;
+            ViewBag.Password = u.Password;
+            ViewBag.BirthDate = u.BirthDate;
+
+            return View();
+        }
+    }
+}
---
+++ 2-12-19CoffeeShop/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace _2_12_19CoffeeShop.Models
+{
+    public class User
+    {
+        [Required] // attributes 
+        [RegularExpression("^[A-Z]+[A-z]{1,30}$")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [RegularExpression("^[a-zA-Z]{2,}$")]
+        public string LastName { get; set; }
+
+        [Required]
+        [RegularExpression(@"^([A-z0-9\.]{5,30})@([A-z]{5,10})\.([a-z]{2,3})$")]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{3}-?\d{3}-?\d{4}$")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-z0-9]{8,30}$")]
+        public string Password { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+
+    }
+}
